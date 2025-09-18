5fb9c4386dae50b58252e13fdb758cfe5a082de9
Reuben Sinha <reuben.sinha@gmail.com>
2025-09-17 19:28:58 -0700

    Login Page!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { useAuth } from './auth/useAuth.ts';
+import Login from './pages/Login.tsx';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
+  const { token } = useAuth();
+
+  if (!token) return <Login />;
 
   return (
     <>
