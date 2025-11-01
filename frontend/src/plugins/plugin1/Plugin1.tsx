627ab480151b3830c4603341c7bf3b22120672ad
reubensinha <74097529+reubensinha@users.noreply.github.com>
2025-11-01 14:13:11 -0700

    Delete Test plugins

--- frontend/src/plugins/plugin1/Plugin1.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "@mantine/core";
-
-function Plugin1() {
-  return (
-    <Text>This is Plugin1 Page</Text>
-  );
-}
-
-export default Plugin1;
--- frontend/src/plugins/plugin1/pages/Plugin1Page.tsx
+++
@@ -1,7 +0,0 @@
-import { Text } from "@mantine/core";
-
-function Plugin1Page() {
-  return <Text>This is Plugin1 Page</Text>;
-}
-
-export default Plugin1Page;
