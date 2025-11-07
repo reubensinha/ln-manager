392a81e0c5780e2b1ebed1067b7ebad358a568e4
Reuben Sinha <reuben.sinha@gmail.com>
2025-11-06 21:28:31 -0800

    moved database to backend/config to persist better during docker rebuilds

--- backend/core/database/database.py
+++ backend/core/database/database.py
@@ -2,7 +2,7 @@ from contextlib import contextmanager
 from sqlmodel import SQLModel, create_engine, Session, select
 
 connect_args = {"check_same_thread": False}
-engine = create_engine("sqlite:///./lnauto.db", echo=False, connect_args=connect_args)
+engine = create_engine("sqlite:///./config/lnauto.db", echo=False, connect_args=connect_args)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
