15509c2a82a86d043767b4635818416bae7a4bcb
Reuben Sinha <reuben.sinha@gmail.com>
2025-11-19 13:25:37 -0800

    Notifications Table is now paginated and sorted in newest first order.

--- frontend/src/pages/System/NotificationsPage.tsx
+++ frontend/src/pages/System/NotificationsPage.tsx
@@ -6,17 +6,32 @@ import { DataTable } from "mantine-datatable";
 import { getNotifications } from "../../api/api";
 import type { Notification } from "../../api/ApiResponse";
 
+const PAGE_SIZES = [10, 15, 20];
+
 function NotificationsPage() {
   const [notifications, setNotifications] = useState<Notification[]>([]);
+  const [pageSize, setPageSize] = useState(PAGE_SIZES[1]);
+  const [page, setPage] = useState(1);
+  const [records, setRecords] = useState(notifications.slice(0, pageSize));
 
   useEffect(() => {
     async function fetchNotifications() {
       const data = await getNotifications();
-      setNotifications(data);
+
+      const sortedData = data.sort((a, b) =>
+        new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
+      );
+      setNotifications(sortedData);
     }
 
     fetchNotifications();
   }, []);
+
+  useEffect(() => {
+    const from = (page - 1) * pageSize;
+    const to = from + pageSize;
+    setRecords(notifications.slice(from, to));
+  }, [page, notifications, pageSize]);
 
   const refreshNotifications = async () => {
     const data = await getNotifications();
@@ -32,12 +47,18 @@ function NotificationsPage() {
       </Group>
       <Divider />
       <DataTable
+        records={records}
         columns={[
           { accessor: "type", title: "Type" },
           { accessor: "message", title: "Message" },
           { accessor: "timestamp", title: "Timestamp" },
         ]}
-        records={notifications}
+        totalRecords={notifications.length}
+        recordsPerPage={pageSize}
+        page={page}
+        onPageChange={(p) => setPage(p)}
+        recordsPerPageOptions={PAGE_SIZES}
+        onRecordsPerPageChange={setPageSize}
       />
     </Stack>
   );
